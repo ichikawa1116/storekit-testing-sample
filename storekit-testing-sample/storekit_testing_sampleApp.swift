8c7900741f0d607436fc4d7763029c6f408164c3
ichikawa1116 <ku.komaruha@icloud.com>
2020-12-13 18:00:47 +0900

    Initial Commit

---
+++ storekit-testing-sample/storekit_testing_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  storekit_testing_sampleApp.swift
+//  storekit-testing-sample
+//
+//  Created by Manami Ichikawa on 2020/12/13.
+//
+
+import SwiftUI
+
+@main
+struct storekit_testing_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ storekit-testing-sampleTests/storekit_testing_sampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  storekit_testing_sampleTests.swift
+//  storekit-testing-sampleTests
+//
+//  Created by Manami Ichikawa on 2020/12/13.
+//
+
+import XCTest
+@testable import storekit_testing_sample
+
+class storekit_testing_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
